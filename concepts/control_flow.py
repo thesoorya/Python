446ce668e2acdf67183da6fe1722ae93b497f7b9
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-05-07 19:09:48 +0530

    control_flow modified

--- concepts/control_flow.py
+++ concepts/control_flow.py
@@ -1,44 +1,55 @@
-# Control flow in programming refers to the order in which statements are executed in a program.
+# # Control flow in programming refers to the order in which statements are executed in a program.
 
-# if statement
+# # if statement
 
-name = input('Enter your name: ')
-age = int(input('Enter your age: '))
+# name = input('Enter your name: ')
+# age = int(input('Enter your age: '))
 
-if age >= 18:
-    print('You are eligible to get license')
-elif age < 18:
-    print('Invalid')
-else:
-    print('You age is not enough')
+# if age >= 18:
+#     print('You are eligible to get license')
+# elif age < 18:
+#     print('Invalid')
+# else:
+#     print('You age is not enough')
 
-# loops
+# # loops
 
-# while - unlimited
-# for - limited
+# # while - unlimited
+# # for - limited
 
-# while loop
-# a statement that will execute its block of code remains true
+# # while loop
+# # a statement that will execute its block of code remains true
 
-f_name = None
-while not(f_name): 
-    f_name = input("Enter your name: ")
+# f_name = None
+# while not(f_name): 
+#     f_name = input("Enter your name: ")
 
-print(f_name)
+# print(f_name)
 
-# for loop
-# a statement runs until the condition is true
+# # for loop
+# # a statement runs until the condition is true
 
-for i in range(10 + 1): #range(start, stop, step)
-    print(i)
+# for i in range(10 + 1): #range(start, stop, step)
+#     print(i)
     
-for i in range(11, 22):
-    print(i)
+# for i in range(11, 22):
+#     print(i)
 
-import time
+# import time
 
-for i in range(10, 0, -1):
-     print(i)
-     time.sleep(1)
+# for i in range(10, 0, -1):
+#      print(i)
+#      time.sleep(1)
      
-print('Boom')
+# print('Boom')
+
+# loop control statements
+# break
+
+while True:
+    name2 = input('Enter your name: ')
+    if name2 != "":
+      break;
+  
+# continue
+# pass
