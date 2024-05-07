dacbe6822654c4e60db909894a4ba3f21d7e47b4
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-05-07 19:02:10 +0530

    control_flow

---
+++ concepts/control_flow.py
@@ -0,0 +1,44 @@
+# Control flow in programming refers to the order in which statements are executed in a program.
+
+# if statement
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+if age >= 18:
+    print('You are eligible to get license')
+elif age < 18:
+    print('Invalid')
+else:
+    print('You age is not enough')
+
+# loops
+
+# while - unlimited
+# for - limited
+
+# while loop
+# a statement that will execute its block of code remains true
+
+f_name = None
+while not(f_name): 
+    f_name = input("Enter your name: ")
+
+print(f_name)
+
+# for loop
+# a statement runs until the condition is true
+
+for i in range(10 + 1): #range(start, stop, step)
+    print(i)
+    
+for i in range(11, 22):
+    print(i)
+
+import time
+
+for i in range(10, 0, -1):
+     print(i)
+     time.sleep(1)
+     
+print('Boom')
