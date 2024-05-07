7705f4bf49f185643f47127a19665b987364203c
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-05-07 16:42:37 +0530

    math_function

---
+++ concepts/math_function.py
@@ -0,0 +1,15 @@
+# math functions are used to perfom some mathematical operations 
+import math
+
+pi = 3.14
+neg = -22
+x= 4
+
+print(math.ceil(pi)) # return the smallest integer greater than or equal to a given number.
+print(math.floor(3.14)) # return the nearest whole number
+print(math.floor(pi)) # convert into integer
+print(abs(neg)) # converts the -ve int0 +ve
+print(math.pow(pi, 2)) # finds the power of the value abs(value, power value)
+print(math.sqrt(pi)) # finds the square root
+print(max(pi, neg, x)) # finds the highest value
+print(min(pi, neg, x)) # finds the minimum value
