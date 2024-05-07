a8962c85a55f24013156d21999199dabc422beeb
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-05-07 11:23:39 +0530

    variables

---
+++ concepts/varialbles.py
@@ -0,0 +1,24 @@
+#string data type
+name = 'soorya'
+print(name)
+print(type(name))
+
+#integer data type (int only store's the whole number cannot store the decimal number)
+age = 21
+age = age + 1 #age +=1 (for increment)
+#print('your age is' + age) we cannot concat str with integer
+print('your age is ' + str(age)) #need to convert the type to concat
+print(age)
+print(type(age))
+
+#float data type
+height = 173.7
+print(height)
+print('your height is ' + str(height) + 'cm')
+print(type(height))
+
+# boolean data type
+student = True
+print(student)
+print(type(student))
+# also works in concat
