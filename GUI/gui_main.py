4a040cf8c52255b841c179d99489837de5f96c69
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-05-09 19:55:41 +0530

    lable

--- GUI/gui_main.py
+++ GUI/gui_main.py
@@ -4,11 +4,26 @@ from tkinter import *
 # widgets - GUI elements
 
 window = Tk()
-window.geometry('400x400') #height and width of the window
+window.geometry('200x200') #height and width of the window
 
-# icon = PhotoImage(file='file.name')
+# icon = PhotoImage(file='logo.jpg')
 # window.iconphoto(icon) #used to change the logo of the window
 
-window.config(background='black')
+window.title('GUI Project')
+
+# window.config(background='black')
+
+########## LABLE
+
+lable = Label(text='Hello Python',
+              font=('Arial',20, 'bold'),
+              padx=20,
+              pady=10,
+              fg='red',
+              bg='black',
+              bd='10',
+              relief='raised'
+              )
+lable.pack()
 
 window.mainloop() #place window on computer and listen for events
